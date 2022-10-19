080cd681e865969d222073688d4b959ce647d2e4
who(true) <alexkjh2011@gmail.com>
2022-10-20 02:15:58 +0900

    Create MemberService.java

---
+++ src/main/java/com/example/demotest/service/MemberService.java
@@ -0,0 +1,10 @@
+package com.example.demotest.service;
+
+import com.example.demotest.domain.Member;
+import com.example.demotest.repository.MemberRepository;
+import com.example.demotest.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService { // 비지니스에 갖고있는 용어를 써야함
