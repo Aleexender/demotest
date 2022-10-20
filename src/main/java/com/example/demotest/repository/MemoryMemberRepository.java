89e3389b8b507d45433f99843277f5cd2985563f
who(true) <alexkjh2011@gmail.com>
2022-10-20 23:51:37 +0900

    make a instances

--- src/main/java/com/example/demotest/repository/MemoryMemberRepository.java
+++ src/main/java/com/example/demotest/repository/MemoryMemberRepository.java
@@ -5,3 +5,7 @@ import com.example.demotest.domain.Member;
 import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L; // 0,1,2 키값을 생성
+
