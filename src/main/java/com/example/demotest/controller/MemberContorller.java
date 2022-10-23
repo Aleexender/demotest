40cac0015d70190f30fadef0bb8bb2bd4756b249
who(true) <alexkjh2011@gmail.com>
2022-10-24 00:54:54 +0900

    Create MemberContorller.java

---
+++ src/main/java/com/example/demotest/controller/MemberContorller.java
@@ -0,0 +1,8 @@
+package com.example.demotest.controller;
+
+import com.example.demotest.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberContorller {
