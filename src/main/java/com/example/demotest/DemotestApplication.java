908750448269d5017bfb2ca3b2f253ae39a06609
who(true) <alexkjh2011@gmail.com>
2022-10-24 00:55:46 +0900

    add information in Main method

--- src/main/java/com/example/demotest/DemotestApplication.java
+++ src/main/java/com/example/demotest/DemotestApplication.java
@@ -3,7 +3,7 @@ package com.example.demotest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 하위 패키치가 아닌애들은 등록이 안댐
 public class DemotestApplication {
 
 	public static void main(String[] args) {
