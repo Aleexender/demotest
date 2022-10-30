2d12055681a4fdab9a6abe76066bda0ee8009a7c
who(true) <alexkjh2011@gmail.com>
2022-10-30 17:26:37 +0900

    add the method and give a @Getmapping("/") (default)

--- src/main/java/com/example/demotest/controller/HomeController.java
+++ src/main/java/com/example/demotest/controller/HomeController.java
@@ -6,3 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @GetMapping("/") // 이거 떄문에 index.html이 무시된다
+    public String home() {
+        return "home";
+    }
+}
