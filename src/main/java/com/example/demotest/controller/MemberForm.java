9831580cac87e294daa19a051c9567c1849c40e7
who(true) <alexkjh2011@gmail.com>
2022-11-01 03:14:09 +0900

    put getter and setter in MemberForm

--- src/main/java/com/example/demotest/controller/MemberForm.java
+++ src/main/java/com/example/demotest/controller/MemberForm.java
@@ -2,3 +2,12 @@ package com.example.demotest.controller;
 
 public class MemberForm {
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
