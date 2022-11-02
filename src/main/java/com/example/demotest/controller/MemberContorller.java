1eba3d176f17443e29cb3eb20548b59a525175e7
who(true) <alexkjh2011@gmail.com>
2022-11-02 22:46:10 +0900

    add information in MemberService

--- src/main/java/com/example/demotest/controller/MemberContorller.java
+++ src/main/java/com/example/demotest/controller/MemberContorller.java
@@ -7,7 +7,8 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MemberContorller {
 
-    private final MemberService memberService;
+
+    private final MemberService memberService; // private 뒤에 @Autowired 를 넣으면 필드 주입, 필드 주입은 바꿀수없기때문에 비추
 
     @Autowired // 연결을 시켜주는것
     public MemberContorller(MemberService memberService) {
