2bf0d9bdd90178adfb7d964b37c61f2da6790d1b
who(true) <alexkjh2011@gmail.com>
2022-11-03 01:16:20 +0900

    add list method and put List to save members

--- src/main/java/com/example/demotest/controller/MemberContorller.java
+++ src/main/java/com/example/demotest/controller/MemberContorller.java
@@ -4,8 +4,11 @@ import com.example.demotest.domain.Member;
 import com.example.demotest.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberContorller {
@@ -24,6 +27,7 @@ public class MemberContorller {
     public String createForm() {
         return "members/createMemberForm";
     }
+
     @PostMapping("/members/new") //데이터를 등록할때 post를 씀
     public String create(MemberForm form) {
         Member member = new Member();
@@ -33,5 +37,13 @@ public class MemberContorller {
 
         memberService.join(member);
         return "redirect:/";       //홈 화면으로 보내버리는거
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members); // 멤버라는 이름으로 멤버객체를 추가
+        return "members/memberList";
+
     }
 }
