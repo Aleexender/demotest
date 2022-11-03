52b20adea32791284ce43c7076bd3cc97ae99360
김민우 <rlaalsdn4564@hanmail.net>
2022-11-03 23:41:49 +0900

    test push

--- src/main/java/com/example/demotest/controller/MemberContorller.java
+++ src/main/java/com/example/demotest/controller/MemberContorller.java
@@ -16,7 +16,7 @@ public class MemberContorller {
 
     private final MemberService memberService; // private 뒤에 @Autowired 를 넣으면 필드 주입, 필드 주입은 바꿀수없기때문에 비추
 
-    @Autowired // 연결을 시켜주는것
+    @Autowired // 연결을 시켜주는것 - 
     public MemberContorller(MemberService memberService) {
 
         this.memberService = memberService;
